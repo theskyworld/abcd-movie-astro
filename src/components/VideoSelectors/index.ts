8d6f05d4e5585c5ff16ee41d947fac15cef63cf0
theskyworld <2456450687@qq.com>
2024-02-04 00:20:33 +0800

    feat: add VideoSelectors

---
+++ src/components/VideoSelectors/index.ts
@@ -0,0 +1,9 @@
+import VideoSelectors from "./VideoSelectors.vue";
+
+export default VideoSelectors;
+
+export type {
+  SelectedItem,
+  VideoSelectorData,
+  VideoSelectorProps,
+} from "./types";
