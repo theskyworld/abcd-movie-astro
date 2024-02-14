71be2b2531ea74b469caede70ef529e64f654c8a
theskyworld <2456450687@qq.com>
2024-02-14 22:01:34 +0800

    refactor: change axios target url, from private ip to public ip

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { AxiosResponse } from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://192.168.1.103:3000/api",
 });
 
 export default function useAxios() {
